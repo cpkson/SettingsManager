b6c1bdd34bcc1fb11aaa88fe9ff4b74ac4cf46c7
Craig <craigp@travelplaces.co.uk>
2015-08-04 16:42:10 +0100

    Initial Commit

---
+++ src/main/java/uk/co/parknet/Settings.java
@@ -0,0 +1,56 @@
+package uk.co.parknet;
+
+import java.util.HashMap;
+
+/**
+ * A class which holds all the settings retrieved from a server
+ * @author CraigP
+ * @version 0.1
+ * @since 3/8/2015
+ */
+public final class Settings 
+{
+	private static Settings instance = null;
+	private HashMap<String,String> settings;
+	
+	/**
+	 * 
+	 */
+	private Settings()
+	{
+		this.settings = new HashMap<String,String>();
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void addSetting(String key, String value)
+	{
+		this.settings.put(key, value);
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return value
+	 */
+	public String getSetting(String key)
+	{
+		return this.settings.get(key);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static Settings getInstance()
+	{
+		if(instance == null)
+		{
+			instance = new Settings();
+		}
+		return instance;
+	}
+}
